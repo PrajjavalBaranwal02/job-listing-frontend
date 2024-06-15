d53595b9c114059a1495c46d8c0f9ddae5cacc85
“PrajjavalBaranwal02” <anujpbaranwal@gmail.com>
2024-06-15 13:17:22 +0530

    First commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,28 @@
-import logo from './logo.svg';
-import './App.css';
+import {Routes, Route, BrowserRouter} from "react-router-dom";
+import RegisterPage from "./pages/RegisterPage/RegisterPage";
+import LoginPage from "./pages/LoginPage/LoginPage";
+import JobPostPage from "./pages/JobPostPage/JobPostPage";
+import JobDescriptionPage from "./pages/JobDescriptionPage/JobDescriptionPage";
+import ProtectedRoute from "./components/ProtectedRoute";
+import HomePage from "./pages/HomePage/HomePage";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/login" element={<LoginPage />} />
+        <Route path="/register" element={<RegisterPage />} />
+        <Route
+          path="/job-post"
+          element={<ProtectedRoute Component={JobPostPage} />}
+        />
+        <Route 
+          path="/job-details/:id"
+          element={<JobDescriptionPage />}
+        />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
